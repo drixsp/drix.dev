4271b6ad4983c3e4b5a24faf54c5f8d329384704
Drix Ponteres <drix@bposeats.com>
2024-03-14 02:25:20 +0800

    Adjust text and spacing

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,43 +1,45 @@
 <template>
     <ContentBody>
-        <div class="top">
-            <h3 class="h3 regular c-tertiary">
-                Lorem ipsum dolor sit amet consectetur
-            </h3>
-            <h1 class="h1 regular">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit
-            </h1>
-            <h1 class="h1 regular">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit
-            </h1>
+        <div class="section">
+            <header class="section-top">
+                <h3 class="h3 regular greeting">
+                    Welcome! I'm Drix.
+                </h3>
+                <h1 class="h1 regular">
+                    Born and raised in Cebu, Philippines.
+                </h1>
+                <h1 class="h1 regular">
+                    Product Designer at HQZen
+                </h1>
+            </header>
+            <main class="section-main">
+                <ul class="nav-list">
+                    <li>
+                        <div class="nav-list__item">
+                            <p>Works</p>
+                            <i class="mdi mdi-arrow-right"></i>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="nav-list__item">
+                            <p>Resume</p>
+                            <i class="mdi mdi-arrow-right"></i>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="nav-list__item">
+                            <p>Contact</p>
+                            <i class="mdi mdi-arrow-right"></i>
+                        </div>
+                    </li>
+                </ul>
+            </main>
+            <footer class="section-footer">
+                <p class="c-tertiary">
+                    Designed and developed by yours truly.
+                </p>
+            </footer>
         </div>
-        <main>
-            <ul class="nav-list">
-                <li>
-                    <div class="nav-list__item">
-                        <p>Item</p>
-                        <p>Icon</p>
-                    </div>
-                </li>
-                <li>
-                    <div class="nav-list__item">
-                        <p>Item</p>
-                        <p>Icon</p>
-                    </div>
-                </li>
-                <li>
-                    <div class="nav-list__item">
-                        <p>Item</p>
-                        <p>Icon</p>
-                    </div>
-                </li>
-            </ul>
-        </main>
-        <footer class="footer">
-            <p class="c-tertiary">
-                Designed and developed by yours truly.
-            </p>
-        </footer>
     </ContentBody>
 </template>
 
@@ -55,34 +57,49 @@
 </script>
 
 <style lang="scss" scoped>
-    .top {
+    // @import 'src/stylesheets/styles.scss';
+
+    .section {
         display: flex;
         flex-direction: column;
-        gap: 12px;
-    }
+        gap: 80px;
 
-    .nav-list {
-        display: flex;
-        flex-direction: column;
-        gap: 24px;
-        margin-top: 80px;
-        width: 50%;
-
-        &__item {
+        &-top {
+            // @include flex-column();
             display: flex;
-            justify-content: space-between;
-            font-size: 36px;
-        }
-    }
+            flex-direction: column;
+            gap: 12px;
 
-    .footer {
-        position: absolute;
-        display: flex;
-        align-items: center;
-        width: 100%;
-        height: 64px;
-        bottom: 0;
-        left: 0;
-        padding: 0 24px;
+            h3 {
+                color: rgba(255, 255, 255, 0.5);
+            }
+        }
+
+        &-main {
+            .nav-list {
+                // @include flex-column();
+                display: flex;
+                flex-direction: column;
+                gap: 24px;
+                width: 50%;
+
+                &__item {
+                    display: flex;
+                    justify-content: space-between;
+                    font-size: 36px;
+                }
+            }
+        }
+
+        &-footer {
+            position: absolute;
+            display: flex;
+            align-items: center;
+            width: 100%;
+            height: 64px;
+            bottom: 0;
+            left: 0;
+            padding: 0 24px;
+        }
     }
 </style>
