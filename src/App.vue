4cce81279bd4aa2f80a36c9b6aa136e523faf497
Drix Ponteres <drix@bposeats.com>
2024-03-21 00:37:10 +0800

    Cleanup code, tmeporarily comment out router-view

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,24 @@
 <template>
     <div class="content">
         <Navbar />
-        <router-view />
-        <!-- <Home /> -->
+        <Home />
     </div>
-    <!-- <Background /> -->
 </template>
 
 <script>
     import Navbar from './components/Navbar.vue'
-    // import Background from './components/Background.vue'
-    // import Home from './pages/Home.vue'
-    // import Works from './pages/Works.vue'
+    import Home from './pages/Home.vue'
 
     export default {
         name: 'App',
         components: {
             Navbar,
-            // Background,
-            // Home,
-            // Works,
+            Home,
         }
     }
 </script>
 
 <style lang="scss">
-    // #app {
-    //     width: 100%;
-    //     height: 100vh;
-    //     background: #171923 !important;
-    //     -webkit-font-smoothing: antialiased;
-    //     -moz-osx-font-smoothing: grayscale;
-    // }
 
     html {
         background: #171923 !important;
